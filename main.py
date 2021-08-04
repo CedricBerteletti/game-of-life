4418386b26cb84b15ce0f83016d0bb254478c020
Cédric Berteletti <cedric.berteletti@gmail.com>
2021-08-04 22:00:58 +0200

    Point d'entrée du programme (main)

---
+++ main.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Cédric Berteletti
+Point d'entrée pour le programme du jeu de la vie
+"""
+
+def main(args):
+    # TODO
+    print("TODO")
+
+if __name__ == "__main__":
+    from sys import argv
+    try:
+        main(argv)
+    except KeyboardInterrupt:
+        pass
